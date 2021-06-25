4161c90a094ae5e6727d702cd19a5b92c1fc4fec
John Edens <dan.edens@geo-instruments.com>
2021-06-25 14:21:41 -0500

    adds sorting function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import shutil
 sorter = os.environ.get('sorting')
 
+IgnoreDownTypes = 'oad,tmp'
+IgnoreFileTypes = 'exe,msi,apk,.gz,tar,gin,zip'
 
 def print_files(directory):
     for filename in os.listdir(directory):
@@ -9,6 +12,22 @@ def print_files(directory):
             continue
         else:
             continue
+
+
+
+
+def sort(dir):
+    for f in os.listdir(dir):
+        FileExtension = f[-3:]
+        if IgnoreDownTypes.find(FileExtension) != -1:
+            continue
+        elif IgnoreFileTypes.find(FileExtension) != 1:
+            shutil.move(sorter+f,eg.globals.jobpath+f)
+            print eg.globals.jobpath+f
+        elif IgnoreFileTypes.find(FileExtension) != -1:
+            shutil.move(sorter+f,'C:\\Users\\Dan.Edens\\Downloads\\')
+            print ("Transfer to Programs complete")
+
 
 
 if __name__ == '__main__':
